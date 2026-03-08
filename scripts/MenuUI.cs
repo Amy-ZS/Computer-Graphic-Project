01eb0dc231a4e600c9116153a4de179a184abf7d
shenqinannnan-creator <shenqinannnan@gmail.com>
2026-03-07 19:02:21 -0500

    Create MenuUI.cs
    Add UI controller for start and end menus

---
+++ scripts/MenuUI.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public partial class MenuUI : CanvasLayer
+{
+    public override void _Ready()
+    {
+        GetNode<Button>("StartMenu/Panel/VBoxContainer/StartButton").Pressed += OnStartPressed;
+        GetNode<Button>("StartMenu/Panel/VBoxContainer/QuitButton").Pressed += OnQuitPressed;
+
+        GetNode<Button>("EndMenu/Panel/VBoxContainer/RestartButton").Pressed += OnRestartPressed;
+        GetNode<Button>("EndMenu/Panel/VBoxContainer/QuitButton").Pressed += OnQuitPressed;
+    }
+
+    private void OnStartPressed()
+    {
+        GameManager.StartGame();
+    }
+
+    private void OnRestartPressed()
+    {
+        GameManager.RestartGame();
+    }
+
+    private void OnQuitPressed()
+    {
+        GameManager.QuitGame();
+    }
+}
