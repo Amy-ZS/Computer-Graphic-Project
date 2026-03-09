d77a53a2ba19374a0e99999b166d93df82a2b095
Larry <larryhe360@gmail.com>
2026-03-08 22:42:34 -0400

    fixed the merge conflicts

--- scripts/MenuUI.cs
+++ scripts/MenuUI.cs
@@ -3,7 +3,6 @@ using System;
 
 public partial class MenuUI : CanvasLayer
 {
-<<<<<<< HEAD
 	public override void _Ready()
 	{
 		GetNode<Button>("StartMenu/Panel/VBoxContainer/StartButton").Pressed += OnStartPressed;
@@ -27,29 +26,4 @@ public partial class MenuUI : CanvasLayer
 	{
 		GameManager.QuitGame();
 	}
-=======
-    public override void _Ready()
-    {
-        GetNode<Button>("StartMenu/Panel/VBoxContainer/StartButton").Pressed += OnStartPressed;
-        GetNode<Button>("StartMenu/Panel/VBoxContainer/QuitButton").Pressed += OnQuitPressed;
-
-        GetNode<Button>("EndMenu/Panel/VBoxContainer/RestartButton").Pressed += OnRestartPressed;
-        GetNode<Button>("EndMenu/Panel/VBoxContainer/QuitButton").Pressed += OnQuitPressed;
-    }
-
-    private void OnStartPressed()
-    {
-        GameManager.StartGame();
-    }
-
-    private void OnRestartPressed()
-    {
-        GameManager.RestartGame();
-    }
-
-    private void OnQuitPressed()
-    {
-        GameManager.QuitGame();
-    }
->>>>>>> 373465ce9f0711d142a6dbb246b5e5dab5554b57
 }
